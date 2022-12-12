1368bdf70ea47892c52051fd7f7666b1fc16fa88
Graciliano-Barroso <grico484@gmail.com>
2022-12-11 21:28:32 -0400

    feat: Transação de Database

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import routes from './routes';
 
 const app = express();
 
-app.use(routes);
-
 app.use(express.json());
+
+app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
